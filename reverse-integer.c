97b0138a01898aec9d7ffcc4f8927330a7862e58
arthur <arthurrabelo2015@hotmail.com>
2025-10-20 16:26:26 -0300

    reverse integer in C

---
+++ reverse-integer.c
@@ -0,0 +1,19 @@
+#include <limits.h>
+
+int reverse(int x){
+    int result = 0;
+    int prev_result = 0;
+
+    while(x != 0){
+        int last_digit = x % 10;
+
+        if(result > INT_MAX / 10 || result < INT_MIN / 10){
+            return 0;
+        }
+
+        result = result * 10 + last_digit;
+        x = x / 10;
+    }
+
+    return result;
+}
